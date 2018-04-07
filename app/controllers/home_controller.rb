38444d79dbcac01da136e00aaf46d45055fba044
Elise Barnes <barnese3@go.stockton.edu>
2018-04-07 15:39:36 -0400

    product test 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
 
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @new_product = ShopifyAPI::Product.new
     @variants = ShopifyAPI::Variant.find(:all)
   end
 
