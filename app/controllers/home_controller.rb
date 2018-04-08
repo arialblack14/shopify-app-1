2d0a8f27d46db4d489a388f543c9b6b971de9e94
Elise Barnes <barnese3@go.stockton.edu>
2018-04-08 11:33:09 -0400

    home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,8 +9,6 @@ class HomeController < ShopifyApp::AuthenticatedController
 		@hello = "hello"
 		@new_product = ShopifyAPI::Product.new
 		@new_product.title = "Burton Custom Freestlye 151"
-		@new_product.product_type = "Snowboard"
-		@new_product.vendor = "nine15-dev-store"
 		@new_product.save
 	end
 
