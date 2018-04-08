2d9eadd4725e2651cdf7978a33d160739ad95edd
Elise Barnes <barnese3@go.stockton.edu>
2018-04-08 11:27:27 -0400

    home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,16 @@ class HomeController < ShopifyApp::AuthenticatedController
 
 	def index
 		@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-		@new_product = ShopifyAPI::Product.new
 		@variants = ShopifyAPI::Variant.find(:all)
 	end
 
 	def hello
 		@hello = "hello"
+		@new_product = ShopifyAPI::Product.new
+		@new_product.title = "Burton Custom Freestlye 151"
+		@new_product.product_type = "Snowboard"
+		@new_product.vendor = "nine15-dev-store"
+		@new_product.save
 	end
 
 end
