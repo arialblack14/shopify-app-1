3a3d12bd88b729f38f844f93166bd211b369d667
Elise Barnes <barnese3@go.stockton.edu>
2018-04-08 12:24:29 -0400

    new product test

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "0338eb2d29688fba312e45aff5e59a25"
   config.secret = "f2d78af8fa1e966e27a2a68411e93f25"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, write_products"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
